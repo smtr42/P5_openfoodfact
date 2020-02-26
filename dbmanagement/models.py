c7800dc9a9bb03cf6369f42d83109455f1a09edc
smtr42 <simteiva@yahoo.fr>
2020-02-26 19:00:07 +0100

    erased unused models

--- dbmanagement/models.py
+++ dbmanagement/models.py
@@ -1,26 +1,6 @@
-# from .database import db
-# from apidata.cleaner import Cleaner
-
-
 class Product:
     def __init__(self, barcode, product_name, nutriscore, url, *args, **kwargs):
         self.barcode = barcode
         self.product_name = product_name
         self.nutriscore = nutriscore
         self.url = url
-
-
-# class Store:
-#     def __init__(self, store_name, *args, **kwargs):
-#         self.store_name = store_name
-#
-#
-# class Category:
-#     def __init__(self, category, *args, **kwargs):
-#         self.category = category
-#
-#
-# class Favorite:
-#     def __init__(self, product_barcode, substitute_barcode, *args, **kwargs):
-#         self.product_barcode = product_barcode
-#         self.substitute_barcode = substitute_barcode
