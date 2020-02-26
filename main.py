a6b62210c2692749e65e1f9b7c82caf95596c9cb
smtr42 <simteiva@yahoo.fr>
2020-02-26 18:59:19 +0100

    modified data check

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 import colorful as cf
 from UI.print_out import input_yes_no, sentence
 from apidata.requester import RequestData
@@ -10,27 +9,16 @@ from dbmanagement.manager.store_manager import StoreManager
 from UI.fsm.main import device
 
 
-def json_exists():
-    """Test the existence of json files"""
-    this_folder = os.path.dirname(os.path.abspath(__file__))
-    data_file = os.path.join(this_folder, 'apidata/products_fr.json')
-    print("data_file :", data_file)
-    print("os path exists :", os.path.exists(data_file))
-    if os.path.exists(data_file):
-        print("It seems you have usable data")
-    else:
-        print(cf.red("IT SEEMS DATA IS MISSING - YOU MUST LET THE APPLICATION "
-                     "DOWNLOAD DATA \n"))
-    answer = input_yes_no(sentence["data"])
-    if answer == 'y':
-        rd = RequestData()
-        rd.fetch_category()
-        rd.fetch_products()
-    else:
-        pass
+print(cf.red("IF IT IS THE FIRST TIME YOU USE THE APPLICATION, YOU MUST "
+             "DOWNLOAD DATA AND RESET DATABASE \n"))
+answer = input_yes_no(sentence["data"])
+if answer == 'y':
+    rd = RequestData()
+    rd.fetch_category()
+    rd.fetch_products()
+else:
+    pass
 
-
-json_exists()
 
 dataclean = Cleaner()
 category_manager = CategoryManager()
