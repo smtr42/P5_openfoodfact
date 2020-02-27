09459c598297519aeea699968c9f2825397c7f43
smtr42 <simteiva@yahoo.fr>
2020-02-27 20:00:15 +0100

    erased comment

--- UI/fsm/state.py
+++ UI/fsm/state.py
@@ -3,16 +3,10 @@ class State(object):
     individual states in the state machine."""
 
     def show(self):
+        """Handle printing function for this state"""
         pass
 
     def on_event(self, event):
         """Handle events that are delegated to this State."""
         pass
-    #
-    # def __repr__(self):
-    #     """Leverages the __str__ method to describe the State."""
-    #     return self.__str__()
-    #
-    # def __str__(self):
-    #     """Returns the name of the State."""
-    #     return self.__class__.__name__
+
